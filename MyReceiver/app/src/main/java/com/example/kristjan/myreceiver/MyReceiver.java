cfc2dab2d6871edfaffb7591ae8782a1013938ae
Ants Kristjan Rooma <antskristjan@hotmail.com>
2016-04-09 14:51:45 +0300

    Receiver project. Implemented onCreate function. Missing manifests for both projects.

---
+++ MyReceiver/app/src/main/java/com/example/kristjan/myreceiver/MyReceiver.java
@@ -0,0 +1,66 @@
+package com.example.kristjan.myreceiver;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.text.DecimalFormat;
+
+public class MyReceiver extends BroadcastReceiver {
+    private double x;
+    private double y;
+    private String operand;
+    private DecimalFormat df;
+    private String result;
+
+    public MyReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (isOrderedBroadcast()){
+            Bundle extras = intent.getExtras();
+
+            if (extras != null) {
+                x = extras.getDouble("x");
+                y = extras.getDouble("y");
+                operand = extras.getString("operand");
+                df = new DecimalFormat("######.######");
+
+                calculate();
+
+            } else {
+                setResultCode(Activity.RESULT_CANCELED);
+            }
+
+            setResultCode(Activity.RESULT_OK);
+            setResultData(result);
+        }
+    }
+
+    private void calculate(){
+        if (operand.equals("+")) {
+            x = x + y;
+
+        } else if (operand.equals("-")) {
+            x = x - y;
+
+        } else if (operand.equals("*")) {
+            x = x * y;
+
+        } else if (operand.equals("/")) {
+            if (y == 0) {
+                result = "Error";
+                return;
+
+            } else {
+                x = x / y;
+            }
+        }
+
+        result = df.format(x);
+    }// private void calculate
+
+}// receiver class
